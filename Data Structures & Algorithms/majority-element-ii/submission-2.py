d2a09212f5c32ce6956904bcfedee7ac3f918e8c
MouniraZitouni <71095730+mouni001@users.noreply.github.com>
2026-04-09 16:42:05 -0400

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]: 
+        dictionary = {}
+        res = []
+        appear = len(nums)/3
+
+        for num in nums:
+            dictionary[num] = dictionary.get(num, 0) + 1
+        for val, freq in dictionary.items():
+            if freq > appear:
+                res.append(val)
+        return res
+        
