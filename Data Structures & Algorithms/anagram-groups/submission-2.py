7fcd368cc64e0db40b4df9d241f392dd95b07777
MouniraZitouni <71095730+mouni001@users.noreply.github.com>
2026-09-15 17:39:42 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_dict = {}
+        for word in strs:
+            sorted_word = tuple(sorted(word))
+            if sorted_word in anagram_dict:
+                anagram_dict[sorted_word].append(word)
+            else:
+                anagram_dict[sorted_word] = [word]
+        return list(anagram_dict.values())
+        
